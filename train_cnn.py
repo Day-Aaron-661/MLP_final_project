dad155a2ced9ece8392dc86b69445b55feef4272
Day-Aaron-661 <aaron.cs12@nycu.edu.tw>
2025-12-10 12:19:36 +0800

    Add files via upload

---
+++ train_cnn.py
@@ -0,0 +1,64 @@
+# train_cnn.py
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+import config as cfg
+import dataset
+import preprocess
+from models import FineTuneModel
+
+def train_cnn_stage():
+
+    all_meta = dataset.load_csv( csv_path=cfg.METADATA_PATH )
+    all_clean_meta = preprocess.get_clean_data( all_meta )
+    
+    # 將 metadata 分成 train 和 test
+    train_meta_df, test_meta_df = dataset.metadata_split( all_clean_meta, cfg.TEST_SPLIT, cfg.RANDOM_SEED )
+
+    train_imgs = dataset.Skin_Datasest(
+        metadata_df=train_meta_df,
+        img_dir=cfg.IMAGE_DIR,
+        mode='train',
+        augmentation=cfg.AUG_CONFIG # 把增強邏輯傳進去
+    )
+    train_loader = DataLoader(train_imgs, batch_size=cfg.BATCH_SIZE, shuffle=True)
+    
+    # 2. 初始化模型
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    print(device)
+    model = FineTuneModel(num_classes=cfg.NUM_CLASS).to(device)
+    
+    # 3. 定義 Loss 和 Optimizer
+    # 針對類別不平衡，可以在 CrossEntropyLoss 加入 weight 參數
+    criterion = nn.CrossEntropyLoss() 
+    optimizer = optim.Adam(model.parameters(), lr=1e-4) # 學習率建議設小一點
+    
+    # 4. 訓練迴圈 (簡單跑個 5-10 Epochs 即可)
+    epochs = 20
+    print(f"開始微調 CNN (共 {epochs} Epochs)...")
+    
+    for epoch in range(epochs):
+        model.train()
+        running_loss = 0.0
+        
+        for images, meta, labels in train_loader: # 注意：這裡暫時用不到 metadata
+            images, labels = images.to(device), labels.to(device, dtype=torch.long)
+            
+            optimizer.zero_grad()
+            outputs = model(images) # 前向傳播
+            loss = criterion(outputs, labels) # 計算 Loss
+            loss.backward() # 反向傳播 (Backpropagation) <-- 這裡就是在更新 CNN 參數！
+            optimizer.step()
+            
+            running_loss += loss.item()
+            
+        print(f"Epoch [{epoch+1}/{epochs}] Loss: {running_loss/len(train_loader):.4f}")
+
+    # 5. 儲存訓練好的權重 (Save Weights)
+    save_path = "fine_tuned_efficientnet.pth"
+    torch.save(model.base_model.state_dict(), save_path)
+    print(f"模型權重已儲存至: {save_path}")
+
+if __name__ == "__main__":
+    train_cnn_stage()
